fb0e64727224b5a59cfc972a1ea955ae6fa79994
Gagan Malvi <malvigagan@gmail.com>
2020-12-25 18:31:57 +0530

    todo: Basic add text functionality
    Signed-off-by: Gagan Malvi <malvigagan@gmail.com>

--- app/src/main/java/com/malviscape/todo/MainActivity.java
+++ app/src/main/java/com/malviscape/todo/MainActivity.java
@@ -2,18 +2,18 @@ package com.malviscape.todo;
 
 import android.os.Bundle;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
 
-import android.view.Menu;
-import android.view.MenuItem;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    private ArrayList<String> items;
+    private android.widget.ArrayAdapter<String> itemsAdapter;
+    private android.widget.ListView ListItems;
+    private android.widget.EditText editToDo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,36 +21,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        ListItems = (android.widget.ListView) findViewById(R.id.ListItems);
+        items = new ArrayList<String>();
+        itemsAdapter = new android.widget.ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, items);
+        ListItems.setAdapter(itemsAdapter);
+        items.add("First Item");
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+    public void fab(View v) {
+        android.widget.EditText editToDo = (android.widget.EditText) findViewById(R.id.editToDo);
+        String itemText = editToDo.getText().toString();
+        itemsAdapter.add(itemText);
+        editToDo.setText("");
     }
 }
+
