184375143fe8ba9f745657670ee7d7a6ff7dd770
Gagan Malvi <malvigagan@gmail.com>
2020-12-25 19:45:13 +0530

    ToDo: Store and retrieve data, and misc fixes.
    Signed-off-by: Gagan Malvi <malvigagan@gmail.com>

--- app/src/main/java/com/malviscape/todo/MainActivity.java
+++ app/src/main/java/com/malviscape/todo/MainActivity.java
@@ -6,7 +6,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
+import android.widget.AdapterView;
 
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,6 +21,26 @@ public class MainActivity extends AppCompatActivity {
     private android.widget.ListView ListItems;
     private android.widget.EditText editToDo;
 
+    private void readToDo() {
+        File filesDir = getFilesDir();
+        File todoFile = new File(filesDir, "todo_contents.cartel");
+        try {
+            items = new ArrayList<String>(FileUtils.readLines(todoFile));
+        } catch (IOException e) {
+            items = new ArrayList<>();
+        }
+    }
+
+    private void saveToDo() {
+        File filesDir = getFilesDir();
+        File todoFile = new File(filesDir, "todo_contents.cartel");
+        try {
+            FileUtils.writeLines(todoFile, items);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,10 +49,10 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         ListItems = findViewById(R.id.ListItems);
         items = new ArrayList<>();
+        readToDo();
         itemsAdapter = new android.widget.ArrayAdapter<>(this,
                 android.R.layout.simple_list_item_1, items);
         ListItems.setAdapter(itemsAdapter);
-        items.add("First Item");
         setupListViewListener();
     }
 
@@ -36,16 +61,18 @@ public class MainActivity extends AppCompatActivity {
         String itemText = editToDo.getText().toString();
         itemsAdapter.add(itemText);
         editToDo.setText("");
+        saveToDo();
     }
 
     private void setupListViewListener() {
         ListItems.setOnItemLongClickListener(
-                new android.widget.AdapterView.OnItemLongClickListener() {
+                new AdapterView.OnItemLongClickListener() {
                     @Override
                     public boolean onItemLongClick(android.widget.AdapterView<?> adapter,
                                                    View item, int pos, long id) {
                         items.remove(pos);
                         itemsAdapter.notifyDataSetChanged();
+                        saveToDo();
                         return true;
                     }
 
