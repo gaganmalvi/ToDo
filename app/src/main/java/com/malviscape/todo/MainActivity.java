3c8d983117a3462a2a5e327cae1cf6041879112c
Gagan Malvi <malvigagan@gmail.com>
2020-12-25 18:40:01 +0530

    ToDo: Add basic remove functionality
    * Simple long-press to remove function
    
    Signed-off-by: Gagan Malvi <malvigagan@gmail.com>

--- app/src/main/java/com/malviscape/todo/MainActivity.java
+++ app/src/main/java/com/malviscape/todo/MainActivity.java
@@ -8,6 +8,7 @@ import androidx.appcompat.widget.Toolbar;
 import android.view.View;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     private ArrayList<String> items;
@@ -21,19 +22,35 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        ListItems = (android.widget.ListView) findViewById(R.id.ListItems);
-        items = new ArrayList<String>();
-        itemsAdapter = new android.widget.ArrayAdapter<String>(this,
+        ListItems = findViewById(R.id.ListItems);
+        items = new ArrayList<>();
+        itemsAdapter = new android.widget.ArrayAdapter<>(this,
                 android.R.layout.simple_list_item_1, items);
         ListItems.setAdapter(itemsAdapter);
         items.add("First Item");
+        setupListViewListener();
     }
 
     public void fab(View v) {
-        android.widget.EditText editToDo = (android.widget.EditText) findViewById(R.id.editToDo);
+        android.widget.EditText editToDo = findViewById(R.id.editToDo);
         String itemText = editToDo.getText().toString();
         itemsAdapter.add(itemText);
         editToDo.setText("");
+    }
+
+    private void setupListViewListener() {
+        ListItems.setOnItemLongClickListener(
+                new android.widget.AdapterView.OnItemLongClickListener() {
+                    @Override
+                    public boolean onItemLongClick(android.widget.AdapterView<?> adapter,
+                                                   View item, int pos, long id) {
+                        items.remove(pos);
+                        itemsAdapter.notifyDataSetChanged();
+                        return true;
+                    }
+
+                });
+
     }
 }
 
